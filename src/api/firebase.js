83e1166e1983cd0e3d9583abd54315d0286d9a8f
Zayn Jarvis <ZaynJarvis@gmail.com>
2019-01-29 21:06:17 +0800

    update firebase app

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -4,27 +4,28 @@ import 'firebase/auth';
 const FirebaseContext = React.createContext(null);
 
 class FirebaseApp {
-	constructor() {
-		const config = {
-			apiKey: process.env.REACT_APP_FIREBASE_TOKEN || '',
-			authDomain: 'hackoss.firebaseapp.com',
-			databaseURL: 'https://hackoss.firebaseio.com',
-			projectId: 'hackoss',
-			storageBucket: 'hackoss.appspot.com',
-			messagingSenderId: '424154730178'
-		};
-		firebase.initializeApp(config);
-		this.auth = firebase.auth();
-	}
-	signin = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
+  constructor() {
+    const config = {
+      apiKey: process.env.REACT_APP_FIREBASE_TOKEN || '',
+      authDomain: 'hackoss-admin.firebaseapp.com',
+      databaseURL: 'https://hackoss-admin.firebaseio.com',
+      projectId: 'hackoss-admin',
+      storageBucket: 'hackoss-admin.appspot.com',
+      messagingSenderId: '841474314837'
+    };
+    firebase.initializeApp(config);
+    this.auth = firebase.auth();
+  }
+  signin = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
 
-	signout = () => this.auth.signOut();
+  signout = () => this.auth.signOut();
 }
 
 const withFirebase = Component => props => (
-	<FirebaseContext.Consumer>
-		{firebase => <Component {...props} firebase={firebase} />}
-	</FirebaseContext.Consumer>
+  <FirebaseContext.Consumer>
+    {firebase => <Component {...props} firebase={firebase} />}
+  </FirebaseContext.Consumer>
 );
 
 export default firebase;
